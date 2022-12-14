2de8c4e84f9b382fe96da4a09b6372f12cd29ca1
Tibarek <ttibarek@gmail.com>
2022-12-14 04:40:50 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Aways 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+
+	int i;
+	
+	i = 0;
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
