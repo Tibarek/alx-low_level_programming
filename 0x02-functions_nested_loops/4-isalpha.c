0956b897637f54f25e5f379fecade975a290e10b
Tibarek <ttibarek@gmail.com>
2022-12-14 12:48:12 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks alphabet
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if alphabet 0 if not
+ */
+int _isalpha(int c)
+{
+	char i;
+
+	char j;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		if (c == i)
+			return (1);
+
+		else
+			i++;
+	}
+	j = 'A';
+	while (j <= 'Z')
+	{
+		if (c == j)
+			return (1);
+		else
+			j++;
+	}
+	return (0);
+}
