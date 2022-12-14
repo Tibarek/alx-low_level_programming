43fd3eefe7dbc2b1c3b839df104da7b4a7431a45
Tibarek <ttibarek@gmail.com>
2022-12-14 13:53:54 -0800

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,12 +3,14 @@
 /**
  * _abs - computes absolute value
  *
+ * @num: number to find absolute value
+ *
  * Return: the positive value
  */
 int _abs(int num)
 
 {
-	if ( num < 0 )
+	if (num < 0)
 		return (num * -1);
 	else
 		return (num);
