42934075c695a27766188b2555bca5246344ad87
Tibarek <ttibarek@gmail.com>
2022-12-14 13:33:33 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign
+ *
+ * @n: number to be checked
+ *
+ * Return: 1 0 or -1 as a sign
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+
+	{
+		_putchar('+');
+		return (1);
+
+	}
+	else if (n == 0)
+	
+	{
+		_putchar('0');
+		return (0);
+
+	}
+	else
+
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
+
