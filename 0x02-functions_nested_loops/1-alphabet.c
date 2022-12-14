3872a66aafbbc88d59cf8d26f2d0e481a3f8c716
Tibarek <ttibarek@gmail.com>
2022-12-14 04:24:28 -0800

    the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: nothig
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+	return;
+}
+
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Aways 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return(0);
+}
