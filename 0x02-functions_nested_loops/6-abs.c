60b2d8feb217ae5ba2964468289ef97cebea7b63
Tibarek <ttibarek@gmail.com>
2022-12-14 13:51:18 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value
+ *
+ * Return: the positive value
+ */
+int _abs(int num)
+
+{
+	if ( num < 0 )
+		return (num * -1);
+	else
+		return (num);
+}
+
