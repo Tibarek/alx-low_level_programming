cff5c137219a462f458269722b2693118eee8eb5
Tibarek <ttibarek@gmail.com>
2022-12-18 00:46:26 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,30 +4,41 @@
  * times_table - prints the 9 times table
  *
  * Return: void
+ * print_long - prints int value
+ *
+ * @value: value to be changed to int
  */
 void print_long(long value)
 {
-	if(value != 0)
+	if( value != 0 )
 	{
-		print_long(value/10);
-		_putchar((value%10 + '0'));
+		print_long(value / 10);
+		_putchar((value % 10 + '0'));
 	}
 }
 void times_table(void)
 {
 	int i, j, k;
 
-	for( i = 0; i <= 9; i++ )
+	for(i = 0; i <= 9; i++)
 	{
-		for( j = 0; j <= 9; j++ )
+		for(j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			if(k == 0)
+			if( k == 0 )
 				_putchar(k + '0');
 			print_long(k);
 			if( j != 9 ) {
 				_putchar(',');
-				_putchar(' ');
+				if (k <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
