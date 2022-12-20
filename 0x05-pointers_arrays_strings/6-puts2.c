7f10c39a3fa733694cc256fc7955423a7842a80a
Tibarek <ttibarek@gmail.com>
2022-12-20 09:41:13 -0800

    Half the lies they tell about me are not true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,14 +12,14 @@ void puts2(char *str)
 	int i, j;
 
 	i = 0;
-	while(*(str + i) != '\0')
+	while (*(str + i) != '\0')
 	{
 		i++;
 	}
 
-	for(j = 0; j < i; j++)
+	for (j = 0; j < i; j++)
 	{
-		if(j % 2 == 0)
+		if (j % 2 == 0)
 		{
 			_putchar(*(str + j));
 		}
