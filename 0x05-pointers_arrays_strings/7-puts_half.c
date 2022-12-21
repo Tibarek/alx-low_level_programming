5cbc8097e54b0fc3d17cc62ffb21febabbd927e2
Tibarek <ttibarek@gmail.com>
2022-12-20 18:19:41 -0800

    half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,6 @@ void puts_half(char *str)
 	{
 		i++;
 	}
-
 	if (i % 2 == 0)
 	{
 		j = i / 2;
@@ -24,10 +23,9 @@ void puts_half(char *str)
 	{
 		j = (i - 1) / 2;
 	}
-
 	while (j < i)
 	{
-		_putchar(*(str + j));
+		_putchar(str[j]);
 		j++;
 	}
 	_putchar('\n');
