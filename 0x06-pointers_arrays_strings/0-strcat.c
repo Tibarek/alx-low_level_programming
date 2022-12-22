9bd8c71cd2f2023e96db7f410be536bdff15a59c
Tibarek <ttibarek@gmail.com>
2022-12-21 17:42:51 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: destination string
+ * @src: source string
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
+	return (dest);
+}
