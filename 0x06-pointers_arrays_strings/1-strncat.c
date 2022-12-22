74faf100738d6cef489a842357c27c39d5468ba9
Tibarek <ttibarek@gmail.com>
2022-12-21 18:02:07 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncat - string concat with n
+ *
+ * @dest: destination string
+ * @src: source string
+ * @n: integer count
+ * Return: concat char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, dlen = 0;
+
+	while (dest[i++])
+		dlen++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[dlen++] = src[i];
+	return (dest);
+}
+	
