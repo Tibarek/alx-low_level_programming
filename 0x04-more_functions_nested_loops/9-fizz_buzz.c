d12a1fed193794a546a530747695cc0a725d5b97
Tibarek <ttibarek@gmail.com>
2022-12-26 09:41:14 -0800

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints fizz buzz for multiples
+ * Return: Success 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -19,3 +19,5 @@ void print_diagonal(int n);
 void print_diagonal(int n);
 
 void print_square(int size);
+
+void fizz_buzz(void);
