7d0da7cfa4c0f8ab1175b3aa284be383688f5d1c
Tibarek <ttibarek@gmail.com>
2022-12-26 09:45:17 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,24 @@ int main(void)
 	{
 		if ((x % 3 == 0) && (x % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (x % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (x % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", x);
+			printf("%d", x);
+		}
+		
+		if (x != 100)
+		{
+			printf(" ");
 		}
 	}
 	return (0);
