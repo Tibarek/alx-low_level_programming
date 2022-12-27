00c0471ea3c40a88c1081f2026dc36d7b97114ea
Tibarek <ttibarek@gmail.com>
2022-12-27 01:11:57 -0800

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes lower to upper
+ * @str: string to changed
+ * Return: pointer to changed string
+ */
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+		{
+			str[index] -= 32;
+		}
+		index++;
+	}
+	return (str);
+}
